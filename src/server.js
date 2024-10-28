22a2511b04234019c80a6ae1a6c769be3583cad8
mevcaus <mevludincausevic@gmail.com>
2024-10-27 23:12:56 -0600

    added server code to get, add, and delete tasks, fixed indentation issue in pug

--- src/server.js
+++ src/server.js
@@ -19,26 +19,37 @@ app.set('view engine', 'pug')
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', function (req, res) {
-    //TODO You will need to do a SQL select here
-    //TODO You will need to update the code below!
-    console.log('GET called')
-    res.render('index')
-
+  const local = { tasks: [] }
+  db.each('SELECT id, task FROM todo', function (err, row) {
+    if (err) {
+      console.log(err)
+    } else {
+      local.tasks.push({ id: row.id, task: row.task })
+    }
+  }, function (err, numrows) {
+    if (!err) {
+      res.render('index', local)
+    } else {
+      console.log(err)
+    }
+  })
 })
 
 app.post('/', function (req, res) {
-    console.log('adding todo item')
-    //TODO You will need to to do a SQL Insert here
-
+  console.log('adding todo item')
+  const stmt = db.prepare('INSERT INTO todo (task) VALUES (?)')
+  stmt.run(req.body.todo)
+  stmt.finalize()
 })
 
 app.post('/delete', function (req, res) {
-    console.log('deleting todo item')
-    //TODO you will need to delete here
-
+  console.log('deleting todo item')
+  const stmt = db.prepare('DELETE FROM todo where id = (?)')
+  stmt.run(req.body.id)
+  stmt.finalize()
 })
 
 // Start the web server
 app.listen(3000, function () {
-    console.log('Listening on port 3000...')
+  console.log('Listening on port 3000...')
 })
